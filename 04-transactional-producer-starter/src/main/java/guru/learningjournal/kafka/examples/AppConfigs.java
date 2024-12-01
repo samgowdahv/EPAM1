f2228f5f0bc498258ce05d78fea066b5cb96ce53
samgowdahv <157664056+samgowdahv@users.noreply.github.com>
2024-12-01 20:52:30 +0530

    Add files via upload

---
+++ 04-transactional-producer-starter/src/main/java/guru/learningjournal/kafka/examples/AppConfigs.java
@@ -0,0 +1,10 @@
+package guru.learningjournal.kafka.examples;
+
+class AppConfigs {
+    final static String applicationID = "HelloProducer";
+    final static String bootstrapServers = "localhost:9092,localhost:9093";
+    final static String topicName1 = "hello-producer-topic1";
+    final static String topicName2 = "hello-producer-topic2";
+    final static int numEvents = 2;
+    final static  String transActionID= "Hello Producer";
+}
